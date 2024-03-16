13908410874a9b8a4cedef7f8bb29d715f1ea821
DevAhmedX <162572837+DevAhmedX@users.noreply.github.com>
2024-03-16 22:12:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int summation(long long num1, long long num2)
+ {
+    return num1 + num2;
+}
+
+int main() {
+  long long num1 , num2 ;
+  cin >> num1 >> num2 ;
+  cout << summation(num1 , num2) ;
+    return 0;
