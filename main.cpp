d0f09067f5e30eb674594cbe377f53d0282503c8
DevAhmedX <162572837+DevAhmedX@users.noreply.github.com>
2024-03-16 22:13:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void printNumbers(int N) {
-    for (int i = 1; i <= N; i++) {
-        cout << i;
-        if (i < N) {
-            cout << " ";
+bool print(int n) {
+   if (n <= 1  )
+    {
+     return false ;
+   }
+
+       for (int i = 2; i <= sqrt(n); i++)
+        {
+        if(n % i == 0){
+            return false ;
         }
-    }
+       }
+
+    return true;
 }
 
 int main() {
-    int N;
-    cin >> N;
-
-    printNumbers(N);
-
+    long long n;
+    cin >> n;
+  if (print(n)) {
+        cout << "YES " << endl;
+    } else {
+        cout << "NO " << endl;
+    }
     return 0;
 }
